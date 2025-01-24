af5045b297b2f8bace403776f78cc37145f3ac6c
221B231 <221B231@juet.co.in>
2025-01-24 14:25:13 +0530

    Adding LE1

---
+++ LE1/PROB1/Mother.java
@@ -0,0 +1,7 @@
+class Mother{
+    int x;
+    
+    void show(){
+        System.out.println("value of x is : " + x);
+    }
+}
---
+++ LE1/PROB4/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Main function");
+		
+		One m1 = new One(1);
+		Two m2 = new Two(12);
+	}
+}
