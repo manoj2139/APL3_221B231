23dea543dc731a5a0997ccab8f6fa6c86f33a4ce
221B231 <221B231@juet.co.in>
2025-01-31 14:29:46 +0530

    Adding LE2

---
+++ LE2/PROB1/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+	public static void main(String[] args) {
+	   
+		Voice v1 = new Voice();
+		v1.prepareVoice();
+		v1.hear();
+	}
+}
---
+++ LE2/PROB2/Test.java
@@ -0,0 +1,7 @@
+public class Test{
+	public static void main(String[] args) {
+	   
+		Voice v1 = new Voice();
+		v1.prepareVoice();
+	}
+}
